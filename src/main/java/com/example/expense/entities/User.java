23cf96c3ad6caa3fe70c931bd7f8fd4f3506b657
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 13:58:38 +0530

    change validation

--- src/main/java/com/example/expense/entities/User.java
+++ src/main/java/com/example/expense/entities/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     int id;
     @NotNull
-    @Size(min = 2, max = 10)
+    @Size(min = 2, max = 20)
     @Column(name = "username")
     String username;
     @Column(name = "nickname")
