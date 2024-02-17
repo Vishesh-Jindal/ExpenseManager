dbac702cc256fa05e8bda814a3fe10637abf794f
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:47:58 +0530

    modify update expense

--- src/main/java/com/example/expense/dal/ExpenseDao.java
+++ src/main/java/com/example/expense/dal/ExpenseDao.java
@@ -1,2 +1,9 @@
-package com.example.expense.dal;public interface ExpenseDao {
+package com.example.expense.dal;
+
+import com.example.expense.entities.Expense;
+
+public interface ExpenseDao {
+    public Expense addExpense(String userId, Expense expense);
+    public Expense updateExpense(String expenseId, Expense expense);
+    public void deleteExpense(String expenseId);
 }
