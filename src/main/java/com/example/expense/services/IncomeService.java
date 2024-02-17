a91ad4017fc8f46f244f1c76a1320857661c26b5
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:51:32 +0530

    add implementation for income service

--- src/main/java/com/example/expense/services/IncomeService.java
+++ src/main/java/com/example/expense/services/IncomeService.java
@@ -1,2 +1,26 @@
-package com.example.expense.services;public class IncomeService {
+package com.example.expense.services;
+
+import com.example.expense.dal.IncomeDao;
+import com.example.expense.entities.Income;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class IncomeService {
+    @Autowired
+    IncomeDao incomeDao;
+    @Transactional
+    public Income addIncome(String userId, Income income){
+        return incomeDao.addIncome(userId, income);
+    }
+    @Transactional
+    public Income updateIncome(String incomeId, Income income){
+        return incomeDao.updateIncome(incomeId, income);
+    }
+    @Transactional
+    public void deleteIncome(String incomeId){
+        incomeDao.deleteIncome(incomeId);
+    }
 }
