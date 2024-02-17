2e547baf7fbedfedea22a158aca1c7c752aab88b
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 12:30:08 +0530

    add query constant

---
+++ src/main/java/com/example/expense/constants/Constants.java
@@ -0,0 +1,7 @@
+package com.example.expense.constants;
+
+public class Constants {
+    public class QueryConstants {
+        public final static String FETCH_BY_USERNAME = "FROM user WHERE user.username= :value";
+    }
+}
