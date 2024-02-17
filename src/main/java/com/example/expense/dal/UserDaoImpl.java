046417ce59c70147ea525777226dd433c70110c3
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 12:30:38 +0530

    add implementation for add user

--- src/main/java/com/example/expense/dal/UserDaoImpl.java
+++ src/main/java/com/example/expense/dal/UserDaoImpl.java
@@ -1,13 +1,29 @@
 package com.example.expense.dal;
 
+import com.example.expense.constants.Constants;
 import com.example.expense.entities.User;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
 
 @Repository
 public class UserDaoImpl implements UserDao{
+    @Autowired
+    EntityManager entityManager;
     @Override
     public User addUser(User user) {
-        return null;
+        Session session = entityManager.unwrap(Session.class);
+        Optional<User> oldUser = Optional.ofNullable(
+                (User) session.createQuery(Constants.QueryConstants.FETCH_BY_USERNAME).setParameter("value",user.getUsername()).getSingleResult()
+        );
+        if(!oldUser.isPresent()){
+            throw new RuntimeException();
+        }
+        Integer incomeId = (Integer)session.save(user);
+        return session.get(User.class,incomeId);
     }
     @Override
     public User updateUser(String userId, User user) {
