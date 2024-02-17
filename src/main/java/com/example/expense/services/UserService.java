2308b57ca858a637206c9f096ddabf1e04f04072
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:52:06 +0530

    add implementation for UserService

--- src/main/java/com/example/expense/services/UserService.java
+++ src/main/java/com/example/expense/services/UserService.java
@@ -1,2 +1,27 @@
-package com.example.expense.services;public class UserService {
+package com.example.expense.services;
+
+import com.example.expense.dal.UserDao;
+import com.example.expense.entities.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class UserService {
+    @Autowired
+    UserDao userDao;
+    @Transactional
+    public User addUser(User user){
+        return userDao.addUser(user);
+    }
+    @Transactional
+    public User updateUser(String userId, User user){
+        return userDao.updateUser(userId, user);
+    }
+    @Transactional
+    public void deleteUser(String userId){
+        userDao.deleteUser(userId);
+    }
+
 }
