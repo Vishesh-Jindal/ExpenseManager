abf795b659a7ae5e287d1be053218e7430357de2
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 19:55:18 +0530

    change column name to userId

--- src/main/java/com/example/expense/entities/Expense.java
+++ src/main/java/com/example/expense/entities/Expense.java
@@ -24,7 +24,7 @@ public class Expense {
     @Column(name = "description")
     String description;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "userId")
     @JsonIgnore
     User user;
     @Enumerated(EnumType.STRING)
--- src/main/java/com/example/expense/entities/Income.java
+++ src/main/java/com/example/expense/entities/Income.java
@@ -24,7 +24,7 @@ public class Income {
     @Column(name = "description")
     String description;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "userId")
     @JsonIgnore
     User user;
     @Enumerated(EnumType.STRING)
