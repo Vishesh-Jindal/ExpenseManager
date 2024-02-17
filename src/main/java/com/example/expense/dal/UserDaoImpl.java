2557d46eebc4152043e034a38681720976244d32
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:50:04 +0530

    create userDaoImpl

--- src/main/java/com/example/expense/dal/UserDaoImpl.java
+++ src/main/java/com/example/expense/dal/UserDaoImpl.java
@@ -1,2 +1,20 @@
-package com.example.expense.dal;public class UserDaoImpl {
+package com.example.expense.dal;
+
+import com.example.expense.entities.User;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao{
+    @Override
+    public User addUser(User user) {
+        return null;
+    }
+    @Override
+    public User updateUser(String userId, User user) {
+        return null;
+    }
+    @Override
+    public void deleteUser(String userId) {
+
+    }
 }
