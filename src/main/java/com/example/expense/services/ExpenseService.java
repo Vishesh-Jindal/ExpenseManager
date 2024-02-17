9ebe82ac331428e3b4df3f218a945c49b92b3fa3
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:51:00 +0530

    add implementation in expense service

--- src/main/java/com/example/expense/services/ExpenseService.java
+++ src/main/java/com/example/expense/services/ExpenseService.java
@@ -1,2 +1,26 @@
-package com.example.expense.services;public class ExpenseService {
+package com.example.expense.services;
+
+import com.example.expense.dal.ExpenseDao;
+import com.example.expense.entities.Expense;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Service
+public class ExpenseService {
+    @Autowired
+    ExpenseDao expenseDao;
+    @Transactional
+    public Expense addExpense(String userId, Expense expense){
+        return expenseDao.addExpense(userId, expense);
+    }
+    @Transactional
+    public Expense updateExpense(String expenseId, Expense expense){
+        return expenseDao.updateExpense(expenseId, expense);
+    }
+    @Transactional
+    public void deleteExpense(String expenseId){
+        expenseDao.deleteExpense(expenseId);
+    }
 }
