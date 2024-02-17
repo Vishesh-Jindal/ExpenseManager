3be574ff28e5bd1b51f19e032b91d1585bb9f037
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 19:20:33 +0530

    add queries to fetch aggregated results

--- src/main/java/com/example/expense/constants/Constants.java
+++ src/main/java/com/example/expense/constants/Constants.java
@@ -3,5 +3,11 @@ package com.example.expense.constants;
 public class Constants {
     public class QueryConstants {
         public final static String FETCH_BY_USERNAME = "FROM User WHERE username=:value";
+        public final static String FETCH_INCOME_BY_YEAR = "SELECT SUM(amount) FROM Income WHERE YEAR(date)=:value";
+        public final static String FETCH_INCOME_BY_MONTH = "SELECT SUM(amount) FROM Income WHERE MONTH(date)=:value";
+        public final static String FETCH_INCOME = "SELECT SUM(amount) FROM Income";
+        public final static String FETCH_EXPENSE_BY_YEAR = "SELECT SUM(amount) FROM Expense WHERE YEAR(date)=:value";
+        public final static String FETCH_EXPENSE_BY_MONTH = "SELECT SUM(amount) FROM Expense WHERE MONTH(date)=:value";
+        public final static String FETCH_EXPENSE = "SELECT SUM(amount) FROM Expense";
     }
 }
