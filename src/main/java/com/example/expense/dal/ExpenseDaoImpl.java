4f0988cc838c0127a293f90411b05f3e049e7be0
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:46:23 +0530

    add implementation for add, update, delete expense

--- src/main/java/com/example/expense/dal/ExpenseDaoImpl.java
+++ src/main/java/com/example/expense/dal/ExpenseDaoImpl.java
@@ -1,2 +1,52 @@
-package com.example.expense.dal;public class ExpenseDaoImpl {
+package com.example.expense.dal;
+
+import com.example.expense.entities.Expense;
+import com.example.expense.entities.User;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.Optional;
+
+@Repository
+public class ExpenseDaoImpl implements ExpenseDao{
+    @Autowired
+    EntityManager entityManager;
+    @Override
+    public Expense addExpense(String userId, Expense expense) {
+        Session session = entityManager.unwrap(Session.class);
+        Optional<User> user = Optional.ofNullable(session.get(User.class,userId));
+        if(!user.isPresent()){
+            throw new RuntimeException();
+        }
+        expense.setUser(user.get());
+        Integer expenseId = (Integer)session.save(expense);
+        return session.get(Expense.class,expenseId);
+    }
+
+    @Override
+    public Expense updateExpense(String expenseId, Expense expense) {
+        Session session = entityManager.unwrap(Session.class);
+        Optional<Expense> oldExpense = Optional.ofNullable(session.get(Expense.class,expenseId));
+        if(!oldExpense.isPresent()){
+            throw new RuntimeException();
+        }
+        oldExpense.get().setDate(expense.getDate());
+        oldExpense.get().setExpenseType(expense.getExpenseType());
+        oldExpense.get().setAmount(expense.getAmount());
+        oldExpense.get().setDescription(expense.getDescription());
+        session.update(oldExpense);
+        return session.get(Expense.class, expenseId);
+    }
+
+    @Override
+    public void deleteExpense(String expenseId) {
+        Session session = entityManager.unwrap(Session.class);
+        Optional<Expense> expense = Optional.ofNullable(session.get(Expense.class,expenseId));
+        if(!expense.isPresent()){
+            throw new RuntimeException();
+        }
+        session.delete(expense.get());
+    }
 }
