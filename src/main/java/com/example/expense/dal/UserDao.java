0847fc947e3487ea914c16636c5d1389e9baa160
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 11:49:27 +0530

    create userDao

--- src/main/java/com/example/expense/dal/UserDao.java
+++ src/main/java/com/example/expense/dal/UserDao.java
@@ -1,2 +1,9 @@
-package com.example.expense.dal;public interface UserDao {
+package com.example.expense.dal;
+
+import com.example.expense.entities.User;
+
+public interface UserDao {
+    public User addUser(User user);
+    public User updateUser(String userId, User user);
+    public void deleteUser(String userId);
 }
