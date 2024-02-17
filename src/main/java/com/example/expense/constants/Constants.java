f52052bd41adbf70e9d804e16ede047bad9e07b7
Vishesh Jindal <visheshjindal@Visheshs-MacBook-Air.local>
2024-02-17 19:53:14 +0530

    add filter on user id

--- src/main/java/com/example/expense/constants/Constants.java
+++ src/main/java/com/example/expense/constants/Constants.java
@@ -3,11 +3,11 @@ package com.example.expense.constants;
 public class Constants {
     public class QueryConstants {
         public final static String FETCH_BY_USERNAME = "FROM User WHERE username=:value";
-        public final static String FETCH_INCOME_BY_YEAR = "SELECT SUM(amount) FROM Income WHERE YEAR(date)=:value";
-        public final static String FETCH_INCOME_BY_MONTH = "SELECT SUM(amount) FROM Income WHERE MONTH(date)=:value";
-        public final static String FETCH_INCOME = "SELECT SUM(amount) FROM Income";
-        public final static String FETCH_EXPENSE_BY_YEAR = "SELECT SUM(amount) FROM Expense WHERE YEAR(date)=:value";
-        public final static String FETCH_EXPENSE_BY_MONTH = "SELECT SUM(amount) FROM Expense WHERE MONTH(date)=:value";
-        public final static String FETCH_EXPENSE = "SELECT SUM(amount) FROM Expense";
+        public final static String FETCH_INCOME_BY_YEAR = "SELECT SUM(amount) FROM Income WHERE YEAR(date)=:year AND user.id=:userId";
+        public final static String FETCH_INCOME_BY_MONTH = "SELECT SUM(amount) FROM Income WHERE MONTH(date)=:month AND user.id=:userId";
+        public final static String FETCH_INCOME = "SELECT SUM(amount) FROM Income WHERE user.id=:userId";
+        public final static String FETCH_EXPENSE_BY_YEAR = "SELECT SUM(amount) FROM Expense WHERE YEAR(date)=:year AND user.id=:userId";
+        public final static String FETCH_EXPENSE_BY_MONTH = "SELECT SUM(amount) FROM Expense WHERE MONTH(date)=:month AND user.id=:userId";
+        public final static String FETCH_EXPENSE = "SELECT SUM(amount) FROM Expense WHERE user.id=:userId";
     }
 }
